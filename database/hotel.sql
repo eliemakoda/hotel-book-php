925fe945954762a446ea0a3024a62f9e2d5c00b4
eliemakodakowo <eliemakodakowo@gmail.com>
2024-02-17 17:55:21 +0100

    end of day 17/02/23 17:55

--- database/hotel.sql
+++ database/hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 17 fév. 2024 à 12:30
+-- Généré le : sam. 17 fév. 2024 à 17:46
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.1.12
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `hotel`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nom_admin` varchar(255) NOT NULL,
+  `email_admin` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `date_ajout` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nom_admin`, `email_admin`, `password`, `telephone`, `sexe`, `date_ajout`) VALUES
+(1, 'Elie makoda ', 'eliemakodakowo@gmail.com', '$2y$10$gv037R96yquIoc.9hY8kV.pXlfSzYKEDrd8N.dbfDgneidkiBBBg2', '+237674349356', 'M', '2024-02-17 14:41:31');
 
 -- --------------------------------------------------------
 
@@ -56,6 +79,36 @@ CREATE TABLE `chambre` (
   `id_hotel` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `chambre`
+--
+
+INSERT INTO `chambre` (`id_chambre`, `num_chambre`, `type_chambre`, `images`, `prix_nuit`, `prix_heure`, `taille_chambre`, `nb_lits`, `statut_chambre`, `id_admin`, `id_hotel`) VALUES
+(1, 'R0402', 'Classique', 'room-4.jpg', '15000', '2000', '123', '1', 0, '1', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `nom_client` varchar(255) NOT NULL,
+  `email_client` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `date_ajout` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom_client`, `email_client`, `password`, `telephone`, `sexe`, `date_ajout`) VALUES
+(1, 'Elie Makoda Kowo ', 'eliemakodakowo@gmail.com', '$2y$10$J.l/MxZCb6AXPmd0LzF0OOkyp8DXsenkcgf7.I1rJgl3PnymxtuV2', '+237674349356', 'M', '2024-02-17 12:49:18');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +126,13 @@ CREATE TABLE `hotel` (
   `id_admin` varchar(255) NOT NULL,
   `date_ajout` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `hotel`
+--
+
+INSERT INTO `hotel` (`id`, `nom_hotel`, `localisation_hotel`, `description_hotel`, `contact_hotel`, `images_hotel`, `nbEtoile_Hotel`, `id_admin`, `date_ajout`) VALUES
+(1, 'La Falaise', 'Douala, Cameroun', 'hôtel situé au cameroun dans la ville de douala offrant un service h24 et 7/7', '+237 698982479', 'services-2.jpg', 5, '1', '2024-02-17 16:03:56');
 
 -- --------------------------------------------------------
 
@@ -115,13 +175,27 @@ CREATE TABLE `service` (
   `type_service` varchar(255) NOT NULL,
   `images_service` varchar(255) NOT NULL,
   `description_service` text NOT NULL,
+  `titre` varchar(255) NOT NULL,
   `id_hotel` varchar(255) NOT NULL,
   `id_admin` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Déchargement des données de la table `service`
+--
+
+INSERT INTO `service` (`id_service`, `type_service`, `images_service`, `description_service`, `titre`, `id_hotel`, `id_admin`) VALUES
+(1, 'Domicile', '2.jpg', 'Nous vous offrons un service traiteur disponible h24 7j/7', 'Service Traiteur', '1', '1');
+
+--
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Index pour la table `avis`
@@ -134,6 +208,12 @@ ALTER TABLE `avis`
 --
 ALTER TABLE `chambre`
   ADD PRIMARY KEY (`id_chambre`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
 
 --
 -- Index pour la table `hotel`
@@ -164,6 +244,12 @@ ALTER TABLE `service`
 --
 
 --
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
@@ -173,13 +259,19 @@ ALTER TABLE `avis`
 -- AUTO_INCREMENT pour la table `chambre`
 --
 ALTER TABLE `chambre`
-  MODIFY `id_chambre` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_chambre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `hotel`
 --
 ALTER TABLE `hotel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `promotion`
@@ -197,7 +289,7 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT pour la table `service`
 --
 ALTER TABLE `service`
-  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
